0c71785c4e9564aa6f1cb73cdfffac343c4fae9a
GustavoHerreroNunes <ghnunes2010@gmail.com>
2022-08-12 14:56:34 -0300

    Edit: Footer and MenuBar responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,24 @@ import Button from './Button';
 const Footer = () => {
     return(
         <footer className="container-flex d-flex justify-content-around bg-dark text-light px-5 pt-4">
-            <Logo />
-            <div>
-                <h3>Desenvolvedor</h3>
-                <a href="https://github.com/GustavoHerreroNunes/">Gustavo Herrero Nunes</a>
-                <div className="d-flex">
-                    <a href="#"><Button className="text-light">Github</Button></a>
-                    <a href="#"><Button className="text-light">Linkedin</Button></a>
-                    <a href="#"><Button className="text-light">Instagram</Button></a>
+            <div className="row d-flex justify-content-evenly w-100">
+                <div className="col-lg-4 col-12 text-center">
+                    <Logo />
+                </div>
+                <div className="col-lg-4 col-sm-6 col-12 text-center">
+                    <h3>Desenvolvedor</h3>
+                    <a href="https://github.com/GustavoHerreroNunes/">Gustavo Herrero Nunes</a>
+                    <div className="d-flex justify-content-evenly">
+                        <a href="#"><Button className="text-light">Github</Button></a>
+                        <a href="#"><Button className="text-light">Linkedin</Button></a>
+                        <a href="#"><Button className="text-light">Instagram</Button></a>
+                    </div>
+                    
+                </div>
+                <div className="col-lg-4 col-sm-6 col-12 text-center">
+                    <span className="fw-bold">Feito com ❤️ para você, cliente!</span>
                 </div>
             </div>
-            <span className="fw-bold">Feito com 💚 para <a href="https://www.frexco.com.br/">Frexco</a></span>
         </footer>
     );
 }
--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -5,19 +5,24 @@ import ShoppingCart from '../assets/shopping-cart.png';
 import { popper } from '@popperjs/core';
 import '../bootstrap-dist/js/bootstrap';
 
-const MenuBar = () => {
-    return(
-        <nav class="navbar navbar-light bg-light">
-            <div class="container">
-                <Logo className="navbar-brand" />
-                <form className="d-flex">
-                    <input className="form-control" type="text" placeholder="Strawberry, banana, apple, etc" />
-                    <Button className="btn-primary fw-bold" image={Magnifier} alt="Pesquisar produto" />
-                </form>
-                <Button image={ShoppingCart} alt="Acessar carrinho de compras">R$:0,00</Button>
+const MenuBar = () => (
+        <nav className="navbar navbar-light bg-light">
+            <div className="row d-flex justify-content-around w-100">
+                <div className="row col-lg-4 col-12 text-center">
+                    <Logo className="navbar-brand" />
+                </div>
+                <div className="row col-lg-4 col-12">
+                    <form className="d-flex">
+                        <input className="form-control" type="text" placeholder="Strawberry, banana, apple, etc" />
+                        <Button className="btn-primary fw-bold" image={Magnifier} alt="Pesquisar produto" />
+                    </form>
+
+                </div>
+                <div className="row col-lg-4 col-12">
+                    <Button image={ShoppingCart} alt="Acessar carrinho de compras">R$:0,00</Button>
+                </div>
             </div>
         </nav>
     );
-}
 
 export default MenuBar;
