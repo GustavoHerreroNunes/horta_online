010b7cc639d4cc929785ee19696bc5aea290925c
GustavoHerreroNunes <ghnunes2010@gmail.com>
2022-10-25 15:08:16 -0300

    Add: last "Your Shopping Cart" page layout components

--- src/pages/YourShoppingCart/index.jsx
+++ src/pages/YourShoppingCart/index.jsx
@@ -1,5 +1,21 @@
-const YourShoppingCart = () => (
-    <h1>Nada aqui por enquanto</h1>
-);
+import Product from "../../components/Product";
+import ShoppingSummary from "../../components/ShoppingSummary";
 
+const YourShoppingCart = () => {
+    return(
+        <div className="container pb-3">
+            <div className="row">
+                <div className="col-md-8 col-12">
+                    <Product id={1} name="Banana Prata" price="10.30" unit="kg" location="YOUR_SHOPPING_CART"  />
+                    <Product id={2} name="Banana Prata" price="10.30" unit="kg" location="YOUR_SHOPPING_CART"  />
+                    <Product id={3} name="Banana Prata" price="10.30" unit="kg" location="YOUR_SHOPPING_CART"  />
+                    <Product id={4} name="Banana Prata" price="10.30" unit="kg" location="YOUR_SHOPPING_CART"  />
+                    <Product id={5} name="Banana Prata" price="10.30" unit="kg" location="YOUR_SHOPPING_CART"  />
+                    <Product id={6} name="Banana Prata" price="10.30" unit="kg" location="YOUR_SHOPPING_CART"  />
+                </div>
+                <ShoppingSummary totalPrice={67.50} />
+            </div>
+        </div>
+    );
+}
 export default YourShoppingCart;
